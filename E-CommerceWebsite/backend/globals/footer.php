eb4bf493830c5802392bbf92437e544766f3736a
Pedro A Cabrera Cordoba <pedrocordobaandres@gmail.com>
2025-11-09 14:54:33 -0500

    Footer Completed. Further Work Required for Header. Styling Contact Page Commenced.

--- E-CommerceWebsite/backend/globals/footer.php
+++ E-CommerceWebsite/backend/globals/footer.php
@@ -1,7 +1,25 @@
 <link rel="stylesheet" href="/Final Project PHPCSS/E-CommerceWebsite/frontend/css/style.css"></link>
 
 <footer> 
+    <section class = "copyWrite">
+        <p>&copy; 2025 DontStealPlease. All rights reserved.</p>
+    </section>
 
+    <nav class = "footerLinks">
+        <a href = "about.php">About Us</a>
+        <a href = "contact.php">Contact Us</a>
+        <a href = "home.php">Return Home</a>
+        <a href = "login.php">Login Here</a>
+        <a href = "register.php">Register Here</a>
+        <a href = "shop.php">Explore</a>
+    </nav>
 
+    <div class = "socialLinks">
+        <p>Follow Us For More On:</p>
+        <a href = "#">Twitter</a>
+        <a href = "#">Facebook</a>
+        <a href = "#">Youtube</a>
+        <a href = "#">Instagram</a>
+    </div>
 
 </footer>
