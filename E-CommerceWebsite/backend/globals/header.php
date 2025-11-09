22857ed06ce7ea4c20e6d695e21af8dfecd7e3f2
Pedro A Cabrera Cordoba <pedrocordobaandres@gmail.com>
2025-11-09 16:26:02 -0500

    HamBurger Nav Rough Complete.
    Work on Home Page starting.

--- E-CommerceWebsite/backend/globals/header.php
+++ E-CommerceWebsite/backend/globals/header.php
@@ -1,5 +1,16 @@
 <link rel="stylesheet" href="/Final Project PHPCSS/E-CommerceWebsite/frontend/css/style.css"></link>
+<script src="/Final Project PHPCSS/E-CommerceWebsite/frontend/jsAnimations/sideNav.js" defer></script>
 <header class = "header">
+    
+<div id= "SideNav" class="sideNav">
+            <a href="javascript:void(0)" class="closeButton" onclick="closeNav()">&times;</a>
+            <a href="#">Shop</a>
+            <a href="#">Login</a>
+            <a href="#">Cart</a>
+            <a href="#">Contact</a>
+        </div>
+        <span class = "openSideNav" onclick="openNav()">=</span>
+
         <nav>
             <ul class = "headerItems">
                 <a href="home.php"> Home </a>
@@ -11,14 +22,6 @@
             </ul>
         </nav>
 
-        <div class="sideNav">
-            <a href="javascript:void(0)" class="closeButton" onclick="closeNav()">&times;</a>
-            <a href="#">Shop</a>
-            <a href="#">Login</a>
-            <a href="#">Cart</a>
-            <a href="#">Contact</a>
-        </div>
-
-        <span onclick="openNav()">open</span>
         
+
     </header>
---
+++ E-CommerceWebsite/frontend/jsAnimations/sideNav.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+  document.getElementById("SideNav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("SideNav").style.width = "0";
+}
