4fcf1e13993b37807caba3cf540a1faadfa8a257
Pedro A Cabrera Cordoba <pedrocordobaandres@gmail.com>
2025-11-11 14:10:02 -0500

    !Important: The ridiculous amount of HTML inside of shop.php is only there as filler, the intention is to have it auto populate based off 1 template of what it is meant to look like. For the time being while the backend is still under construction that will remain to fill the page for what it is meant to look like(rough), by the end.

--- E-CommerceWebsite/frontend/pages/shop.php
+++ E-CommerceWebsite/frontend/pages/shop.php
@@ -13,9 +13,315 @@
    <?php include "C:\\xampp\htdocs\Final Project PHPCSS\E-CommerceWebsite\backend\globals\header.php"; ?>
 
     <main>
-        <section>
-            
-        </section>       
+        <section class = "shopBanner">
+            <h1> Spooktober Flash Sale! </h1>
+            <h3>Discounts up to 20% off</h3>
+            <div class="dividerShopPage"></div>
+
+        </section>
+
+        <section class = "shop">
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div> 
+                
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div> 
+                
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div> 
+                
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div> 
+                
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div> 
+                
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+                <div class = "shopItems"> <!-- 5 Rows 3 Columns -->
+                    <img src="/Final Project PHPCSS/E-CommerceWebsite/assets(temp)/TestImagePHPCSS.jpg" alt="A really cool guy" class = "productImage">
+                    <p class="itemType">Autofill Item Filters ie, women fashion, luxury etc</p>
+                    <h3 class = "itemName"> Iron Mask</h3>
+                    <div class = "productCardDivider"></div>
+                    <p class = "itemPrice"> $20,000.00 </p>
+                    
+
+                    <div class = "productButtonGroup">
+                     <button type="button" > View </button>
+                     <button type="button" > Add To Cart </button>
+                     <button type="button" > Favorite </button>   
+                    </div>
+                </div>  
+
+
     </main>
 
         <?php include "C:\\xampp\htdocs\Final Project PHPCSS\E-CommerceWebsite\backend\globals\\footer.php" ?>
