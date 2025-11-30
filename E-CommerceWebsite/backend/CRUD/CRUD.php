f7b9dfceef1b065469fd90fd69ef999130f8cff3
Pedro A Cabrera Cordoba <pedrocordobaandres@gmail.com>
2025-11-30 09:28:22 -0500

    Input Validation to be refined, temporary stand in complete.
    Create Skeleton to be made.

--- E-CommerceWebsite/backend/CRUD/CRUD.php
+++ E-CommerceWebsite/backend/CRUD/CRUD.php
@@ -5,6 +5,32 @@ require_once ("../config.php");
 // ^^^^^ Neccessary Imports Above Here ^^^^^
 
 // Create Stuff Below 
+class CRUD {
+    private $connl;
+    private $table_name="form_data";
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function dataValidation($data) {
+        $err = [];
+
+        if(empty($data['name'])) {
+            $err['name'] = "Name required.";
+        } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $data['name'])) {
+            $err['name'] = "Letters and white spaces only for Name.";
+        }
+    }
+}
+
+    if (empty($data['email'])) {
+            $err['email'] = "Email is requires";
+    } elseif(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+        $ERR['email'] = "This is not a valid email format.";
+    }
+
+    
 
 
 
